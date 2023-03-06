dfdb578881f8c8eff58606f4479b34cb522bdde1
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-06 15:48:58 +0530

    Refactor The Code to Use Lamda Function to Validate User Entry

--- EmailValidate.cs
+++ EmailValidate.cs
@@ -12,9 +12,24 @@ namespace UserRegistrationDemo
         public bool ValidateEmail(string email)
         {
 
-            string pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$";
-            Regex regex = new Regex(pattern);
-            return regex.IsMatch(email);
+            // Define lambda functions for validation
+            Func<string, bool> validateFirstName = name => Regex.IsMatch(name, @"^[A-Za-z]{2,20}$");
+            Func<string, bool> validateLastName = name => Regex.IsMatch(name, @"^[A-Za-z]{2,20}$");
+            Func<string, bool> validateEmail = email => Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+            Func<string, bool> validateMobile = mobile => Regex.IsMatch(mobile, @"^[0-9]{10}$");
+            Func<string, bool> validatePassword = password => Regex.IsMatch(password, @"^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[^\da-zA-Z]).{8,20}$");
+
+            // Test the lambda functions
+            Console.WriteLine(validateFirstName("John")); // true
+            Console.WriteLine(validateFirstName("J")); // false
+            Console.WriteLine(validateLastName("Doe")); // true
+            Console.WriteLine(validateLastName("D")); // false
+            Console.WriteLine(validateEmail("john.doe@example.com")); // true
+            Console.WriteLine(validateEmail("john.doe@.com")); // false
+            Console.WriteLine(validateMobile("1234567890")); // true
+            Console.WriteLine(validateMobile("123456")); // false
+            Console.WriteLine(validatePassword("Passw0rd!")); // true
+            Console.WriteLine(validatePassword("password")); // false
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -6,29 +6,25 @@ namespace UserRegistrationDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter an email address : ");
-            string email = Console.ReadLine();
+            // Define lambda functions for validation
+            Func<string, bool> validateFirstName = name => Regex.IsMatch(name, @"^[A-Za-z]{2,20}$");
+            Func<string, bool> validateLastName = name => Regex.IsMatch(name, @"^[A-Za-z]{2,20}$");
+            Func<string, bool> validateEmail = email => Regex.IsMatch(email, @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$");
+            Func<string, bool> validateMobile = mobile => Regex.IsMatch(mobile, @"^[0-9]{10}$");
+            Func<string, bool> validatePassword = password => Regex.IsMatch(password, @"^(?=.[a-z])(?=.[A-Z])(?=.\d)(?=.[^\da-zA-Z]).{8,20}$");
 
-
-            if (ValidateEmail(email))
-            {
-                Console.WriteLine("The email address is valid.");
-            }
-            else
-            {
-                Console.WriteLine("The email address is invalid.");
-            }
+            // Test the lambda functions
+            Console.WriteLine(validateFirstName("John")); // true
+            Console.WriteLine(validateFirstName("J")); // false
+            Console.WriteLine(validateLastName("Doe")); // true
+            Console.WriteLine(validateLastName("D")); // false
+            Console.WriteLine(validateEmail("john.doe@example.com")); // true
+            Console.WriteLine(validateEmail("john.doe@.com")); // false
+            Console.WriteLine(validateMobile("1234567890")); // true
+            Console.WriteLine(validateMobile("123456")); // false
+            Console.WriteLine(validatePassword("Passw0rd!")); // true
+            Console.WriteLine(validatePassword("password")); // false
         }
-        static bool ValidateEmail(string email)
-        {
-
-            string emailRegex = @"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$";
-
-
-            Match match = Regex.Match(email, emailRegex);
-
-
-            return match.Success;
-        }
+        
     }
 }
