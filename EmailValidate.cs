cefdfe6a832f17d71ddbf218cc9c7b9a5806b67e
“codewitherrors” <codewitherrors1@gmail.com>
2023-03-06 14:51:12 +0530

    Add : Write testcase for user Entry firstname ,lastname , etx

---
+++ EmailValidate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationDemo
+{
+    public class EmailValidate
+    {
+       
+
+        public string validate(string firstName,string lastName,String email,string mobile,string password)
+        {
+            if(firstName== "" && lastName == "" && email == "" && mobile == "" && password == "") 
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,30 +7,7 @@ namespace UserRegistrationDemo
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Enter an email address : ");
-            string email = Console.ReadLine();
 
-
-            if (ValidateEmail(email))
-            {
-                Console.WriteLine("The email address is valid.");
-            }
-            else
-            {
-                Console.WriteLine("The email address is invalid.");
-            }
-        }
-
-        static bool ValidateEmail(string email)
-        {
-
-            string emailRegex = @"^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$";
-
-
-            Match match = Regex.Match(email, emailRegex);
-
-
-            return match.Success;
         }
     }
 }
